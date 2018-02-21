7db2395008b3f10631f161c01a8310e62f040ff2
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-02-21 09:43:14 +0100

    add api key and remove console.log

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -4,11 +4,9 @@ import React from 'react';
 
 import withGeolocation from '../hocs/withGeolocation';
 
-navigator.geolocation.getCurrentPosition(console.log, console.error);
-
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
+    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAOu0YNN7QMQuY-Ki9bK0KwLYN9jVP78nM&libraries=geometry,drawing,places',
     loadingElement: <div style={{ height: '100%' }} />,
     containerElement: <div style={{ height: 'calc(100vh - 64px)' }} />,
     mapElement: <div style={{ height: '100%' }} />,
